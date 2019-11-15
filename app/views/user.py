2377facc9ce11433ccc7a299e9d59e0d2d7313ab
Harsh <h.x.dev@outlook.com>
2019-11-15 17:51:24 -0500

    Remove signup UI

--- app/views/user.py
+++ app/views/user.py
@@ -12,38 +12,6 @@ ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 userbp = Blueprint("userbp", __name__, url_prefix="/user")
 
 
-@userbp.route("/signup", methods=["GET", "POST"])
-def signup():
-    form = user_forms.SignUp()
-    if form.validate_on_submit():
-        # Create a user who hasn't validated his email address
-        user = models.User(
-            first_name=form.first_name.data,
-            last_name=form.last_name.data,
-            phone=form.phone.data,
-            email=form.email.data,
-            confirmation=False,
-            password=form.password.data,
-        )
-        # Insert the user in the database
-        db.session.add(user)
-        db.session.commit()
-        # Subject of the confirmation email
-        subject = "Please confirm your email address."
-        # Generate a random token
-        token = ts.dumps(user.email, salt="email-confirm-key")
-        # Build a confirm link with token
-        confirmUrl = url_for("userbp.confirm", token=token, _external=True)
-        # Render an HTML template to send by email
-        html = render_template("email/confirm.html", confirm_url=confirmUrl)
-        # Send the email to user
-        email.send(user.email, subject, html)
-        # Send back to the home page
-        flash("Check your emails to confirm your email address.", "positive")
-        return redirect(url_for("index"))
-    return render_template("user/signup.html", form=form, title="Sign up")
-
-
 @userbp.route("/confirm/<token>", methods=["GET", "POST"])
 def confirm(token):
     try:
