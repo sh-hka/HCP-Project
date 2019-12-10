1991f3e7f791d1f1571010023758c2ba66e6a61a
Harsh <h.x.dev@outlook.com>
2019-12-09 22:20:02 -0500

    Fix getter and setter for password
    Note: It is still planned to remove the signup/login feature.

--- app/models.py
+++ app/models.py
@@ -24,12 +24,18 @@ class User(db.Model, UserMixin):
     def password(self):
         return self._password
 
-    @password.setter
     def _set_password(self, plaintext):
         self._password = bcrypt.generate_password_hash(plaintext)
 
+    def _get_password(self):
+        # Hopefully this is salted and hashed
+        # TODO: Check to see if this is hashed or not.
+        return self.password
+
     def check_password(self, plaintext):
         return bcrypt.check_password_hash(self.password, plaintext)
+
+    password = db.synonym('_password', descriptor=property(_get_password,_set_password))
 
     def get_id(self):
         return self.email
