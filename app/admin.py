6be875e80b572be29f80fb7a3cf6f6655390571e
Harsh <h.x.dev@outlook.com>
2019-12-10 01:18:13 -0500

    Add Applications and Provider Tables to admin dashboard
    + Also removes the user page.

--- app/admin.py
+++ app/admin.py
@@ -7,7 +7,8 @@ from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.fileadmin import FileAdmin
 
 from app import app, db
-from app.models import User
+from app.provider import Provider
+from app.application import Application
 
 admin = Admin(app, name="Admin", template_mode="bootstrap3")
 
@@ -17,19 +18,23 @@ class ModelView(ModelView):
         auth = request.authorization or request.environ.get(
             "REMOTE_USER")  # workaround for Apache
         if (not auth or
-            (auth.username, auth.password) != app.config["ADMIN_CREDENTIALS"]):
+                (auth.username, auth.password) != app.config["ADMIN_CREDENTIALS"]):
             raise HTTPException(
                 "",
                 Response(
-                    "You have to an administrator.",
+                    "You have to be an administrator.",
                     401,
                     {"WWW-Authenticate": 'Basic realm="Login Required"'},
                 ),
             )
         return True
 
-# Users
-admin.add_view(ModelView(User, db.session))
+
+# Applications
+admin.add_view(ModelView(Application, db.session))
+
+# Providers
+admin.add_view(ModelView(Provider, db.session))
 
 # Static files
 path = op.join(op.dirname(__file__), "static")
