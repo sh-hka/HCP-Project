fd3d63ecb4e94f9d82f06d7900594e5cf0ad38d8
Harsh <h.x.dev@outlook.com>
2019-12-09 22:18:44 -0500

    Implement remaining fields of Provider and export it.

---
+++ app/views/provider/__init__.py
@@ -0,0 +1,1 @@
+from app.views.provider.models import Provider
--- app/views/provider/models.py
+++ app/views/provider/models.py
@@ -5,5 +5,19 @@ class Provider(db.Model):
 
     __tablename__ = 'providers'
 
-    id = db.Column(db.Integer, nullable=False, autoincrement=True)
+    id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    speciality = db.Column(db.String)
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    zip = db.Column(db.String, nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    lng = db.Column(db.Float, nullable=False)
 
+    __table_args__ = (
+        db.CheckConstraint(-90 <= lat, name="Latitude lower range check"),
+        db.CheckConstraint(lat <= 90, name="Latitude upper range check"),
+        db.CheckConstraint(-180 <= lng, name="Longitude lower range check"),
+        db.CheckConstraint(lng <= 180, name="Longitude upper range check"),
+        {})
