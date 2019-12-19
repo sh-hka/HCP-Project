5a54b1c690651cb70a941453938030cd7f740f8f
Harsh <h.x.dev@outlook.com>
2019-12-19 04:44:38 -0500

    Add a bit of the search management logic

--- app/search/views.py
+++ app/search/views.py
@@ -1,4 +1,19 @@
-from flask import Blueprint, render_template, jsonify
+from app.search.SearchQuery import (
+    SearchQuery,
+    BadQueryException,
+)
+
+from flask import (
+    Blueprint,
+    render_template,
+    jsonify,
+    request,
+)
+
+from flask_api import status
+from urllib.parse import unquote
+
+import json
 
 # Create a search blueprint
 searchbp = Blueprint("searchbp", __name__)
@@ -11,4 +26,19 @@ def search():
 
 @searchbp.route('/search', methods=['POST'])
 def results():
+    query_data: dict = request.json
+    if query_data is None:
+        return jsonify([{}])
+    query = query_data.get('query', None)
+    position = query_data.get('position', None)
+    search_range = query_data.get('range', None)
+    try:
+        search_query = SearchQuery(
+            query=query,
+            position=position,
+            search_range=search_range
+        )
+    except BadQueryException as e:
+        response = jsonify(error=e.get_msg()), status.HTTP_400_BAD_REQUEST
+        return response
     return jsonify([{}])
