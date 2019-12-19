cc985c4309189e34f6cac49466c6b27f0034fab5
Kangan <31378646+sh-hka@users.noreply.github.com>
2019-12-18 23:20:13 -0500

    searchpage layout

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -26,3 +26,39 @@ function search() {
     const url = '/search?data=' + encodeURIComponent(query);
     window.location = url;
 }
+
+
+var map;
+function placeMarkerMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
+
+  // Create a <script> tag and set the USGS URL as the source.
+  var script = document.createElement('script');
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+  window.eqfeed_callback = function(results) {
+    for (var i = 0; i < results.features.length; i++) {
+      var coords = results.features[i].geometry.coordinates;
+      var latLng = new google.maps.LatLng(coords[1],coords[0]);
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+    }
+  }
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=gmaps_api_key&callback=initMap">
+</script>
+
+
