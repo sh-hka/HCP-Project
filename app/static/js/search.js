d56a218a56394e4be8d4796601abbccfa3f2d001
Kangan <31378646+sh-hka@users.noreply.github.com>
2019-12-19 23:14:54 -0500

    Add files via upload
    search page layout

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -27,38 +27,96 @@ function search() {
     window.location = url;
 }
 
+var matched_providers = [];
 
-var map;
-function placeMarkerMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
-  });
+function search_results(data) {
+    data = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
 
-  // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            clear_results_list();
+            var results_list = JSON.parse(this.responseText);
+            show_results(results_list);
+        }
+    });
+
+    xhr.open("POST", "http://localhost:5000/search");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-  window.eqfeed_callback = function(results) {
-    for (var i = 0; i < results.features.length; i++) {
-      var coords = results.features[i].geometry.coordinates;
-      var latLng = new google.maps.LatLng(coords[1],coords[0]);
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-      });
+function show_results(result_list) {
+    let result_list_div;
+    result_list_div = document.getElementById('results-list');
+    for (let i = 0; i < result_list.length; i++) {
+        var result = result_list[i];
+        result_div = get_result_div(result);
+        result_marker = get_result_marker(result);
+        // TODO: Store it in a global list
+        result_list_div.appendChild(result_div);
     }
-  }
-</script>
-<script async defer
-src="https://maps.googleapis.com/maps/api/js?key=gmaps_api_key&callback=initMap">
-</script>
+}
 
+function get_result_div(result) {
+    var address = result.address+','+result.city+"\n"+
+                 result.state+','+result.zip;
+    var content = document.createElement('div');
+    content.classList.add('result');
+    content.classList.add('item');
+    content.classList.add('ui');
+    content.classList.add('card');
+    content.classList.add('content');
+    var p0 = document.createElement('div');
+    content.classList.add('header');
+    p0.innerText = result.name;
+    var p1 = document.createElement('p');
+    content.classList.add('small');
+    content.classList.add('feed');
+    content.classList.add('event');
+    content.classList.add('content');
+    content.classList.add('summary');
+    p1.innerText = result.speciality;
+    var p2 = document.createElement('p');
+    content.classList.add('event');
+    content.classList.add('content');
+    content.classList.add('summary');
+    p2.innerText = address;
+    // var p3 = document.createElement('p');
+    // p3.innerText = result.contact;
+    var bttn = document.createElement('button');
+    content.classList.add('extra');
+    content.classList.add('content');
+    content.classList.add('button');
+    bttn.innerHTML = "Apply";
+    bttn.onclick = function () {
+        apply_now(result.id);
+    };
+    content.appendChild(p0); content.appendChild(p1); content.appendChild(p2); content.appendChild(bttn);
+    return content;
+}
+
+function get_result_marker(result) {
+   // placeMarkerMap(result.lat, result.lng);
+}
+
+
+function clear_results_list() {
+    for (let i = 0; i < matched_providers.length; i++) {
+        // TODO: Add removal logic here.
+    }
+}
+
+function apply_now(id) {
+    const url = '/apply?provider=' + id;
+    window.location = url;
+}
+
+
+
+// </script>
+// <script async defer
+// src="https://maps.googleapis.com/maps/api/js?key=gmaps_api_key&callback=initMap">
+// </script>
 
