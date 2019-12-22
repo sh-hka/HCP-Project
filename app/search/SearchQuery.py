ba372085eae64e9ca9a20bec2ea3a8cf78c8fd1d
Harsh <h.x.dev@outlook.com>
2019-12-22 16:11:29 -0500

    working Search Engine

--- app/search/SearchQuery.py
+++ app/search/SearchQuery.py
@@ -1,9 +1,12 @@
+from app.provider import Provider
+from math import cos, pi, asin
+
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
-
 from geocoder import google as google_geocode
 
-from app.provider import Provider
+from sqlalchemy import or_, and_
+
 
 ENTITY_TYPE = enums.Entity.Type
 
@@ -11,6 +14,7 @@ EXCLUDE_ENTITY_TYPES = {ENTITY_TYPE.PHONE_NUMBER, ENTITY_TYPE.ADDRESS, ENTITY_TY
                         ENTITY_TYPE.DATE, ENTITY_TYPE.EVENT, ENTITY_TYPE.PRICE}
 
 COMMON_STRINGS = {"doctor", "hospital", "doctors", "hospitals"}
+
 
 class BadQueryException(Exception):
     def __init__(self, msg=None):
@@ -29,31 +33,52 @@ class SearchQuery:
         self.query = query
         self.position = position
         self.range = search_range
+        if position is not None and position.keys() != {'lat', 'lng'}:
+            raise BadQueryException("Malformed SearchQuery")
         if query is None or query == '':
-            if position is None:
-                raise BadQueryException("Empty SearchQuery not permitted")
-            elif position.keys() != {'lat', 'lng'}:
+            if position is None or range is None:
                 raise BadQueryException("Malformed SearchQuery")
-            self.position = position
+            self.parsed_query = {'address': None, 'locations': [], 'strings': []}
         else:
             self.parsed_query = self.__parse_query(query)
 
     def search(self, provider: Provider.__class__):
         query = provider.query
+        # Strings
+        string_filters = []
+        strings = self.parsed_query['strings']
+        for col in [provider.name, provider.address, provider.speciality]:
+            string_filters.append(or_(col.ilike(string) for string in strings))
+        if len(strings) > 0 and len(string_filters) > 0:
+            query = query.filter(or_(*string_filters))
+        # Location
         if self.parsed_query['address'] is not None:
-            # Address - single address/area
-            pass
+            bbox = self.parsed_query['address'].bbox
+            query = query.filter(self.__in_bbox(provider, bbox))
+            matches = [match.to_dict for match in query.limit(30).all()]
         elif len(self.parsed_query['locations']) > 0:
             # Locations - Union of locations
+            location_filters = []
             for location in self.parsed_query['locations']:
-                pass
-        else: # IE. Query string didn't yield any location based filter
-            pass
+                bbox = location.bbox
+                location_filters.append(self.__in_bbox(provider, bbox))
+            query = query.filter(or_(*location_filters))
+            matches = [match.to_dict() for match in query.limit(30).all()]
+        elif self.position is not None and self.range is not None:
+            def dist(lat, lng):
+                p = pi/180
+                lat1, lng1 = self.position['lat']*p, self.position['lng']*p
+                lat, lng = lat*p, lng*p
+                a = 0.5 - (cos((lat - lat1))/2) + (cos(lat) * cos(lat1) * (1 - cos(lng - lng1))/2)
+                return 12742 * asin(a**0.5)
+            matches = [match.to_dict() for match in query.all()]
+            matches = list(filter(lambda m: dist(m['lat'], m['lng']) <= self.range, matches))
+        return matches
 
-        for string in self.parsed_query['strings']:
-            pass
-        matches: Provider = query.limit(30).all()
-        return [match.to_dict() for match in matches]
+    @staticmethod
+    def __in_bbox(provider: Provider.__class__, bbox):
+        (n, e), (s, w) = bbox['northeast'], bbox['southwest']
+        return and_(provider.lat <= n, provider.lat >= s, provider.lng <= e, provider.lng >= w)
 
     @staticmethod
     def __parse_query(query):
