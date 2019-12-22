deea30f1ce372c239739a79c4e69b7d7c2b2fb37
Harsh <h.x.dev@outlook.com>
2019-12-22 04:32:43 -0500

    Search Engine basics up and running.
    Like a boss

--- app/search/SearchQuery.py
+++ app/search/SearchQuery.py
@@ -1,6 +1,15 @@
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
 
+from geocoder import google as google_geocode
+
+from app.provider import Provider
+
+ENTITY_TYPE = enums.Entity.Type
+
+EXCLUDE_ENTITY_TYPES = {ENTITY_TYPE.PHONE_NUMBER, ENTITY_TYPE.ADDRESS, ENTITY_TYPE.LOCATION, ENTITY_TYPE.NUMBER,
+                        ENTITY_TYPE.DATE, ENTITY_TYPE.EVENT, ENTITY_TYPE.PRICE}
+
 
 class BadQueryException(Exception):
     def __init__(self, msg=None):
@@ -28,17 +37,46 @@ class SearchQuery:
         else:
             self.parsed_query = self.__parse_query(query)
 
+    def search(self, provider: Provider.__class__):
+        query = provider.query
+        if self.parsed_query['address'] is not None:
+            # Address - single address/area
+            pass
+        elif len(self.parsed_query['locations']) > 0:
+            # Locations - Union of locations
+            for location in self.parsed_query['locations']:
+                pass
+        else: # IE. Query string didn't yield any location based filter
+            pass
+
+        for string in self.parsed_query['strings']:
+            pass
+        matches: Provider = query.limit(30).all()
+        return [match.to_dict() for match in matches]
+
     @staticmethod
     def __parse_query(query):
         type_ = enums.Document.Type.PLAIN_TEXT
         document = {'content': query, 'type': type_}
         encoding_type = enums.EncodingType.UTF8
 
-        nlp_response = SearchQuery.NLP_CLIENT.analyze_entities(document, encoding_type=encoding_type)
+        nlp_entities = SearchQuery.NLP_CLIENT.analyze_entities(document, encoding_type=encoding_type).entities
 
-        # TODO: See the response and generate a dict of all the important stuff
+        address_entity = [e.name for e in nlp_entities if e.type == ENTITY_TYPE.ADDRESS]
+        address_geocode = google_geocode(address_entity[0]) if len(address_entity) > 0 else None
+        address = None
+        if address_geocode is not None and address_geocode.ok:
+            address = address_geocode.current_result
+        location_entities = [e.name for e in nlp_entities if e.type == ENTITY_TYPE.LOCATION]
+        locations = []
+        if address is None and len(location_entities) > 0:
+            locations_geocode = [google_geocode(loc) for loc in location_entities]
+            locations = [loc.current_result for loc in locations_geocode if loc.ok]
+        other_entities = [e.name for e in nlp_entities if e.type not in EXCLUDE_ENTITY_TYPES]
 
         parsed_query = {
-            'locale': {'state': None, 'city': None, 'zip': None},
+            'address': address,
+            'locations': locations,
+            'strings': other_entities
         }
         return parsed_query
--- app/search/views.py
+++ app/search/views.py
@@ -3,6 +3,8 @@ from app.search.SearchQuery import (
     BadQueryException,
 )
 
+from app.provider import Provider
+
 from flask import (
     Blueprint,
     render_template,
@@ -28,7 +30,8 @@ def search():
 def results():
     query_data: dict = request.json
     if query_data is None:
-        return jsonify([{}])
+        match = Provider.query.limit(30).all()
+        return jsonify([provider.to_dict() for provider in match])
     query = query_data.get('query', None)
     position = query_data.get('position', None)
     search_range = query_data.get('range', None)
@@ -38,6 +41,7 @@ def results():
             position=position,
             search_range=search_range
         )
+        return jsonify(search_query.search(Provider))
     except BadQueryException as e:
         response = jsonify(error=e.get_msg()), status.HTTP_400_BAD_REQUEST
         return response
